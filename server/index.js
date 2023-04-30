ae3469d0a136c3aa88ca772c78aa9705bb21f7bb
vrajsoni98 <vrajsoni727@gmail.com>
2023-04-30 00:26:46 -0400

    v1.0 for Live Polls

---
+++ server/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const fs = require("fs").promises;
+const path = require("path");
+
+const app = express();
+const dataFile = path.join(__dirname, "data.json");
+
+//support POSTing from data wwith URL encoded
+app.use(express.urlencoded({ extended: true }));
+
+//Enable CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.get("/poll", async (req, res) => {
+  let data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
+  const totalVotes = Object.values(data).reduce((total, n) => (total += n), 0);
+
+  data = Object.entries(data).map(([label, votes]) => {
+    return {
+      label,
+      votes,
+      percentage: (totalVotes === 0 || votes === 0) ? 0 : ((100 * votes) / totalVotes).toFixed(1),
+    };
+  });
+
+  res.json(data);
+});
+
+app.post("/poll", async (req, res) => {
+  const data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
+  console.log("hello",data);
+  data[req.body.add]++;
+
+  await fs.writeFile(dataFile, JSON.stringify(data));
+
+  res.end();
+});
+
+app.post("/poll/reset", async (req, res) => {
+  const data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
+
+  for (const prop in data) {
+    data[prop] = 0;
+  }
+
+  await fs.writeFile(dataFile, JSON.stringify(data));
+
+  res.end();
+});
+
+app.listen(3000, () => console.log("Server is running..."));
